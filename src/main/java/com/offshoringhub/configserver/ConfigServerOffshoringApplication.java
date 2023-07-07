ab70a43e7753802203848a3e26faf65f0561d88a
anasmoaya <anassmoayaa@gmail.com>
2023-07-07 17:36:54 +0100

    first commit

---
+++ src/main/java/com/offshoringhub/configserver/ConfigServerOffshoringApplication.java
@@ -0,0 +1,13 @@
+package com.offshoringhub.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerOffshoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerOffshoringApplication.class, args);
+    }
+
+}
